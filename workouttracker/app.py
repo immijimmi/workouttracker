daa35e359da5deeac5b65847748b3f44fef85590
immijimmi <imranhamid99@msn.com>
2022-07-20 03:58:41 +0100

    Temporarily removed Tk.lift() - it does not seem to be functioning as used here

--- workouttracker/app.py
+++ workouttracker/app.py
@@ -12,7 +12,6 @@ class App:
         self._window.minsize(width=200, height=0)  # Min width so that the window remains grabbable using the cursor
         self._window.title(Constants.WINDOW_TITLE)
         self._window.iconbitmap(config.ICON_FILE_PATH)
-        self._window.lift()
 
         # Make the window expand to fit displayed content
         self._window.columnconfigure(0, weight=1)

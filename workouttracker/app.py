595d0b3129bbb2ebc8c71f16895b4ff5fe03d2af
immijimmi <imranhamid99@msn.com>
2022-07-20 03:49:55 +0100

    Minor restructure in App, added Tk.lift() to ensure that the window is pushed in front of other windows on startup

--- workouttracker/app.py
+++ workouttracker/app.py
@@ -8,10 +8,11 @@ from .config import Config
 class App:
     def __init__(self, config=Config):
         self._window = Tk()
+        self._window.resizable(False, False)
+        self._window.minsize(width=200, height=0)  # Min width so that the window remains grabbable using the cursor
         self._window.title(Constants.WINDOW_TITLE)
         self._window.iconbitmap(config.ICON_FILE_PATH)
-
-        self._window.minsize(width=200, height=0)  # Min width so that the window remains grabbable using the cursor
+        self._window.lift()
 
         # Make the window expand to fit displayed content
         self._window.columnconfigure(0, weight=1)
@@ -20,5 +21,4 @@ class App:
         self.tracker = Tracker(self._window, config)
         self.tracker.render().grid(sticky="nswe")
 
-        self._window.resizable(False, False)
         self._window.mainloop()

c01bd9463901965685babe9db48c6256200dc992
immijimmi <imranhamid99@msn.com>
2022-09-14 03:11:04 +0100

    Updated start-venv.bat to ensure requirements are installed before running application

--- tools/start-venv.bat
+++ tools/start-venv.bat
@@ -1,5 +1,8 @@
 :: Runs the application in the development venv, with no command line arguments and without displaying a terminal
+:: Automatically ensures any dependencies are installed before running
 :: Requires a venv with compatible dependencies for the project to be installed under \venv
 
 cd ..
+venv\Scripts\python -m pip install -r requirements.txt
+
 start venv\Scripts\pythonw -m workouttracker

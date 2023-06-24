145f39c64f5a20a42a5fce85809c9457131783ab
immijimmi <immijimmi1@gmail.com>
2023-06-24 03:29:51 +0100

    Updated tool

--- tools/start-dev.bat
+++ tools/start-dev.bat
@@ -1,8 +1,9 @@
-:: Runs the application in the development venv, with no command line arguments and without displaying a terminal
+:: Runs the application in the development venv, with no command line arguments and leaving the terminal visible even in the event of a crash
 :: Automatically ensures any dependencies are installed before running
 :: Requires a venv with compatible dependencies for the project to be installed under \venv
 
 cd ..
 venv\Scripts\python -m pip install -r requirements.txt
 
-start venv\Scripts\pythonw -m workouttracker
+venv\Scripts\python -m workouttracker
+pause

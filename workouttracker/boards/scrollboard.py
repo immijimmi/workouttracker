70ee960645a6a802372b66211e627fd2121c17f4
immijimmi <immijimmi1@gmail.com>
2023-07-30 03:30:12 +0100

    Corrected comment into TODO comment

---
+++ workouttracker/boards/scrollboard.py
@@ -0,0 +1,54 @@
+from tkcomponents.basiccomponents import ScrollFrame
+from tkcomponents.extensions import GridHelper
+
+from abc import ABC
+
+from ..constants import Constants as TrackerConstants
+
+
+class ScrollBoard(ScrollFrame.with_extensions(GridHelper), ABC):
+    def __init__(self, tracker, container, update_interval_ms=None):
+        self._tracker = tracker
+        self._state = self.tracker.state
+        self._theme = self.tracker.config.Theme
+
+        board_style_args = self.theme.BOARD_STYLE_ARGS.get(
+            type(self).__name__,
+            self.theme.STANDARD_STYLES["board_args"]
+        )
+
+        super().__init__(
+            container,
+            lambda: self.width_clearance,
+            is_vertical_scroll=False,
+            update_interval_ms=update_interval_ms,
+            styles={
+                "frame": {
+                    "borderwidth": TrackerConstants.BORDERWIDTH_TINY,
+                    "relief": "sunken",
+                    "bg": board_style_args["bg"],
+                    "padx": TrackerConstants.PAD_SMALL,
+                    "pady": TrackerConstants.PAD_SMALL
+                }
+            }
+        )
+
+        self.styles["board_args"] = {
+            **board_style_args
+        }
+
+    @property
+    def tracker(self):
+        return self._tracker
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def theme(self):
+        return self._theme
+
+    @property
+    def display_name(self) -> str:
+        raise NotImplementedError
--- workouttracker/config.py
+++ workouttracker/config.py
@@ -22,7 +22,7 @@ class Config:
         BoardController: {"row": 0, "column": 0, "rowspan": 3},
         Actuals: {"row": 0, "column": 1, "rowspan": 2, "columnspan": 7},
         Schedule: {"row": 2, "column": 1, "rowspan": 2, "columnspan": 8},
-        # Tips: {"row": 4, "column": 1, "columnspan": 5},  # Tips are disabled until further notice
+        # Tips: {"row": 4, "column": 1, "columnspan": 5},  ##### TODO: Tips are disabled until further notice
         File: {"row": 0, "column": 8, "rowspan": 2, "columnspan": 4},
         SchedulesEditor: {"row": 2, "column": 9, "rowspan": 2, "columnspan": 6},
         WorkoutsEditor: {"row": 4, "column": 1, "rowspan": 3, "columnspan": 12},
